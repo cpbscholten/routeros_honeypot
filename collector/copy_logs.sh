4455ef4fbf8f63cb9b969f58806e4a8ee7e69aa1
Christian Scholten <scholten.cpb@gmail.com>
2020-01-14 15:06:19 +0100

    Update copy_logs.sh

--- collector/copy_logs.sh
+++ collector/copy_logs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # retrieve the logs from a honeypot and store them on the collector 
 source_dir=/home/honeypot/logs/
-dest_dir=/home/collector/logs/
+dest_dir=/home/collector/logs/${honeypot_name}/
 # arguments used when calling script
 # name of honeypot, the ip address of the honeypot
 honeypot_name=$1
 ip=$2
 echo "Copying new logs to local:"
 # the --remove-source-files argument is used to ensure that the honeypot will not run out of space.
-eval "rsync -auvv honeypot@${ip}:${source_dir} ${dest_dir}${honeypot_name}/ --remove-source-files"
+eval "rsync -auvv honeypot@${ip}:${source_dir} ${dest_dir} --remove-source-files"
